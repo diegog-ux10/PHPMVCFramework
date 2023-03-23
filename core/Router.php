bed878c04a6419fa6469c7231055fbbcd378ea85
Diego Granados <diego@ceicom.com.br>
2023-03-23 07:25:31 -0400

    ÃÂ¨Âviews¨

--- core/Router.php
+++ core/Router.php
@@ -4,16 +4,23 @@ namespace app\core;
 class Router
 {
     public Request $request;
+    public Response $response;
     protected array $routes = [];
 
-    public function __construct(\app\core\Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
     {
         $this->routes['get'][$path] = $callback;
+    }
+
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
     }
 
     public function resolve()
@@ -23,9 +30,34 @@ class Router
 
         $callback = $this->routes[$method][$path] ?? false;
         if ($callback === false) {
-            echo "Not found";
-            exit;
+            $this->response->setStatusCode(404);
+            return $this->renderView("_404");
         }
-        echo call_user_func($callback);
+        if (is_string($callback)) {
+            return $this->renderview($callback);
+        }
+        return call_user_func($callback);
     }
+
+    public function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/$view.php";
+        return ob_get_clean();
+    }
+
 }
